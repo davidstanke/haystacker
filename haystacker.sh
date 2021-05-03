0c39c9bd40f232f9759918c21bf87c98f2831379
davidstanke <davidstanke@gmail.com>
2021-05-03 15:52:43 +0000

    implement querying

--- haystacker.sh
+++ haystacker.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
 set -euo pipefail
 
+# TODO: parse these variables from arguments
+DATE_START="2021-05-02"
+DATE_END="2021-05-03"
+INCREMENT_IN_HOURS=6
+
+# DEV ONLY
+rm -rf matches/*
+mkdir -p matches
+
 function utc_time () {
     echo $(date +"%Y-%m-%dT%H:%M:%SZ" -d $1)
 }
 
-startDate="2021-01-29"
-endDate="2021-05-02"
+# convert dates to timestamps
+TIMESTAMP_START=$(date -d "${DATE_START}T00:00:00Z" "+%s")
+TIMESTAMP_END=$(date -d "${DATE_END}T00:00:00Z" "+%s")
 
-startTime=$(date -d "${startDate}T00:00:00Z" "+%s")
-endTime=$(date -d "${endDate}T00:00:00Z" "+%s")
+# from TIMESTAMP_START to TIMESTAMP_END, query one increment at a time
+TIMESTAMP_QUERY_START=$TIMESTAMP_START
+while [[ $TIMESTAMP_QUERY_START -le $TIMESTAMP_END ]]; do
+    TIMESTAMP_QUERY_END=$((TIMESTAMP_QUERY_START+(INCREMENT_IN_HOURS*3600)))
+    QUERY_START_UTC=$(utc_time @${TIMESTAMP_QUERY_START})
+    QUERY_END_UTC=$(utc_time @${TIMESTAMP_QUERY_END})
 
-echo "startTime: $startTime"
-echo "endTime: $endTime"
+    echo "querying: ${QUERY_START_UTC} to ${QUERY_END_UTC}"
 
-# from startTime to endTime, query one hour at a time
-queryTime=$startTime
-while [[ $queryTime -le $endTime ]]; do
-    queryEnd=$((queryTime+3600))
-    echo "querying: $queryTime to $queryEnd"
+    matches=$(gcloud logging read "timestamp >= \"${QUERY_START_UTC}\" AND timestamp < \"${QUERY_END_UTC}\" AND \"1w9t17kg1x0ixus\"")
 
-    # gcloud logging read "timestamp >= \"$(utc_time ${queryTime})\" AND timestamp < \"$(utc_time ${queryEnd})\" AND labels.\"k8s-pod/app\"=\"currencyservice\"" \
-    # > matches/$(date -d "$queryTime" "+%Y%m%d%H%M%S").txt
+    if [ "${matches}" != "" ]; then
+        echo "${matches}" > matches/$(date -d "@${TIMESTAMP_QUERY_START}" "+%Y%m%d%H%M%S").txt
+    fi
 
-    utcStart=$(utc_time ${queryTime})
-    utcEnd=$(utc_time ${queryEnd})
-    echo "utcStart: $utcStart"
-    echo "utcEnd: $utcEnd"
-
-    # echo "gcloud logging read timestamp >= \"$(utc_time ${queryTime})\" AND timestamp < \"$(utc_time ${queryEnd})\" AND labels.\"k8s-pod/app\"=\"currencyservice\""
-
-    queryTime=$queryEnd
+    TIMESTAMP_QUERY_START=$TIMESTAMP_QUERY_END
 done
-
-echo "final querytime: $queryTime"
 
 
